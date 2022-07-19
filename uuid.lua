8ab52f134a5c432bb218e1e061cbb1d37c9d2214
Felipe Izolan <felipeizolann@gmail.com>
2022-07-19 03:40:56 -0300

    Update uuid.lua

--- uuid.lua
+++ uuid.lua
@@ -1,14 +1,16 @@
 --[[
+  uuid - v4
   x = A char in lowercase
   y = A char in uppercase
   z = A number
 --]]
 
-local split = require "scripts.misc.split"
-local char_lowercase = split("qwertyuiopasdfghjklzxcvbnm")
-local char_uppercase = split("QWERTYUIOPASDFGHJKLZXCVBNM")
-local char_number = split("1234567890")
-local char = {"x", "y", "z"}
+local split = require "split"
+
+local lowercase = split("qwertyuiopasdfghjklzxcvbnm")
+local uppercase = split("QWERTYUIOPASDFGHJKLZXCVBNM")
+local number = split("1234567890")
+local template_char = {"x", "y", "z"}
 
 function Template()
   local template = ""
@@ -17,22 +19,22 @@ function Template()
     if i == 9 or i == 14 or i == 19 or i == 24 then
       template = template .. "-"
     else
-      template = template .. char[math.random(1, #char)]
+      template = template .. template_char[math.random(1, #template_char)]
     end
   end
 
   return template
 end
 
-function Id()
+function Uuid()
   local template = Template()
   local id = string.gsub(template, ".", function(c)
-    if c == "x" then return char_lowercase[math.random(1, #char_lowercase)] end
-    if c == "y" then return char_uppercase[math.random(1, #char_uppercase)] end
-    if c == "z" then return char_number[math.random(1, #char_number)] end
+    if c == "x" then return lowercase[math.random(1, #lowercase)] end
+    if c == "y" then return uppercase[math.random(1, #uppercase)] end
+    if c == "z" then return number[math.random(1, #number)] end
   end)
 
   return id
 end
 
-return Id, Template
+return Uuid, Template
